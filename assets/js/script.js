96ab848f40b6cc00fc1d3d30a7ea5086f75c1bc1
on2onyekachi <kachiokwuolisa@gmail.com>
2025-07-17 23:16:02 +0100

    Reduce padding on mobile responsive view

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,4 @@ document.addEventListener('DOMContentLoaded', function () {
         themeIcon.classList.toggle('fa-moon');
         themeIcon.classList.toggle('fa-sun');
     });
-
-    // Scroll Animations
-    const animateElements = document.querySelectorAll('.scroll-animate');
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('visible');
-                    observer.unobserve(entry.target);
-                }
-            });
-        },
-        { threshold: 0.2 }
-    );
-
-    animateElements.forEach((el) => observer.observe(el));
 });
